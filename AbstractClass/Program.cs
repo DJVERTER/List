33ee5e1e15aa86e4f49ec50cd37d3b79f4cab279
DJVERTER <62717168+DJVERTER@users.noreply.github.com>
2021-10-14 12:14:13 +0300

    Add project files.

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        public static double Seconds { get; private set; }
+
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() { };
+            int sum = 0;
+            {
+                num.Add(5);
+                num.Add(7);
+                num.Add(10);
+            }
+
+            foreach (var item in num)
+            
+                sum += item;
+                Console.WriteLine($"Number in collection: {sum}");
+            
+        }
+    }
+}
